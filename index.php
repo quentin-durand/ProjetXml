d5d4042efff246da14b586480d6ec62d97839f52
Duwelzdorian <duwelzdorian@gmail.com>
2019-06-11 18:16:45 +0200

    navbar & css

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
+
+
 <!DOCTYPE html>
  <html lang="fr" dir="ltr">
  <head>
 <meta charset="utf-8" />
     <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel = "stylesheet"
+       href = "https://fonts.googleapis.com/icon?family=Material+Icons">
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -14,17 +17,59 @@
 
   </head>
 
+  <!-- <div class="title">Super</div>
+  <div class="subtitle">Lecteur RSS</div> -->
+
+
   <body>
 
 
+    <!-- **** NAVBAR ****-->
+    <div class="navbar-fixed">
+        <nav>
+          <div class="nav-wrapper grey darken-4">
+            <ul class="hide-on-med-and-down menuAccueil">
+              <li><a href="option-1.html">Accueil</a></li>
+            </ul>
+            <!-- Logo -->
+            <a href="#!" class="hide-on-med-and-down brand-logo"><img src="logo.png"></a>
+            <a href="#!" class="hide-on-large-only brand-logo"><img src="logo.png"></a>
+            <!-- MENU -->
+            <ul class="hide-on-med-and-down menuText">
+            	<li><a href="option-1.html" class="">Sécurité</a></li>
+              <li><a href="applis.xml">Application</a></li>
+            	<li><a href="option-1.html" class="">Autre</a></li>
+            </ul>
+<!-- LOGIN -->
+
+<ul class="hide-on-med-and-down right">
+  <li><a href="sass.html" class="menuLogin"><i class="material-icons left">
+    account_circle</i>Login</a></li>
+  <li><a href="badges.html" class="menuLogin"><i class="material-icons left">lock</i>S'enregistrer</a></li>
+          </ul>
+          </div>
+        </nav>
+      </div>
 
 
+  <div class="separatorNav1"></div>
+  <div class="separatorNav2"></div>
 
- 
+
+  <!-- Footer -->
+  <footer>
+  <div class="footer-copyright">
+    <div class="container">
+      <span class="footerText">© 2019. Quentin / Dorian</span>
+    </div>
+  </div>
+  </footer>
+
+
 
     <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="script.js"></script>
     </body>
 </html>
-
