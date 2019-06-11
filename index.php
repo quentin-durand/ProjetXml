3a614de3141c2b39c942652135f237b827baee22
quentin-durand <q.durand8@gmail.com>
2019-06-11 09:47:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+ <html lang="fr" dir="ltr">
+ <head>
+<meta charset="utf-8" />
+    <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link type="text/css" rel="stylesheet" href="style.css"/>
+<title>Projet Xml</title>
+
+  </head>
+
+  <body>
+
+
+
+
+
+ 
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="script.js"></script>
+    </body>
+</html>
+
